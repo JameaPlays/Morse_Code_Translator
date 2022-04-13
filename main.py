2788aac59c70e506f35bf837af8c70b89fe625f6
James Chua <chua.yijin35@hotmail.com>
2022-04-13 19:20:02 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,58 @@
+# Morse Code Dictionary
+TEXT_TO_MORSE_DICT = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+                      'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+                      'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+                      'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+                      '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ', ': '--..--',
+                      '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-', ' ': ' '}
+
+MORSE_TO_TEXT_DICT = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',
+                      '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P',
+                      '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+                      '-.--': 'Y', '--..': 'Z', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5',
+                      '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0', '--..--': ', ',
+                      '.-.-.-': '.', '..--..': '?', '-..-.': '/', '-....-': '-', '-.--.': '(', '-.--.-': ')', ' ': ' '}
+
+
+def text_to_morse():
+    user_input = input('Type the message to be converted to Morse Code.\n').upper()
+    converted_text = ""
+    for char in user_input:
+        if char == " ":
+            converted_text += "| "
+        else:
+            converted_text += TEXT_TO_MORSE_DICT[char]
+            converted_text += " "
+    return converted_text
+
+
+def morse_to_text():
+    user_input = input('Type the message to be converted to Text.\n'
+                       'Use "|" to separate each character.\n')
+    input_list = user_input.split(" ")
+    # return input_list
+    converted_text = ""
+    for char in input_list:
+        if char == " ":
+            pass
+        elif char == '|':
+            converted_text += " "
+        else:
+            converted_text += MORSE_TO_TEXT_DICT[char]
+    return converted_text
+
+
+converter_mode = input("A) Text to Morse Code\n"
+                       "B) Morse Code to Text\n"
+                       "What would you like to convert? Type A or B: ").upper()
+
+running = True
+while running:
+    if converter_mode == "A":
+        print(text_to_morse())
+        break
+    elif converter_mode == "B":
+        print(morse_to_text())
+        break
+    else:
+        print("Invalid input, please try again.")
